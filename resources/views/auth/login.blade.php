75447155ba401e3b401b10cb24e495a70addb82c
mberry87 <berry311287@gmail.com>
2023-06-06 00:26:47 +0700

    fix file dan create auth

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,73 +1,67 @@
-@extends('layouts.app')
+@extends('layouts.auth')
+
+@section('title', 'Login')
+
+@section('logo')
+    <div class="login-logo">
+        <a href="#">
+            <p>Kantor KSOP <br>
+                <span>Kelas II Tanjungpinang</span>
+            </p>
+        </a>
+    </div>
+@endsection
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
+    <div class="card-body login-card-body">
+        <p class="login-box-msg">Silahkan Login</p>
 
-                <div class="card-body">
-                    <form method="POST" action="{{ route('login') }}">
-                        @csrf
-
-                        <div class="row mb-3">
-                            <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="row mb-3">
-                            <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="row mb-3">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
-                                </button>
-
-                                @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
-                                    </a>
-                                @endif
-                            </div>
-                        </div>
-                    </form>
+        <form action="{{ route('login') }}" method="POST">
+            @csrf
+            <div class="input-group mb-3">
+                <input id="username" type="text" name="username"
+                    class="form-control @error('username') is-invalid @enderror" placeholder="Username"
+                    value="{{ old('username') }}">
+                <div class="input-group-append">
+                    <div class="input-group-text">
+                        <span class="fas fa-user"></span>
+                    </div>
                 </div>
+                @error('username')
+                    <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                @enderror
             </div>
-        </div>
+            <div class="input-group mb-3">
+                <input type="password" name="password" class="form-control @error('password') is-invalid @enderror"
+                    placeholder="Password" value="{{ old('password') }}">
+                <div class="input-group-append">
+                    <div class="input-group-text">
+                        <span class="fas fa-lock"></span>
+                    </div>
+                </div>
+                @error('password')
+                    <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                @enderror
+            </div>
+            <div class="row">
+                <div class="col-8">
+                    <p class="mb-1">
+                        <a href="forgot-password.html">Lupa password</a>
+                    </p>
+                </div>
+                <!-- /.col -->
+                <div class="col-4">
+                    <button type="submit" class="btn btn-primary btn-block">{{ __('Login') }}</button>
+                </div>
+                <!-- /.col -->
+            </div>
+            <p class="mb-0">
+                <a href="{{ route('register') }}" class="text-center">{{ __('Register') }}</a>
+            </p>
+        </form>
     </div>
-</div>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Auth\LoginController;
 use Illuminate\Support\Facades\Auth;
 use App\Http\Controllers\BenderaController;
 use App\Http\Controllers\DashboardCotroller;
@@ -28,68 +29,72 @@ Route::get('/', function () {
 });
 
 Auth::routes();
-// Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-
-Route::get('/dashboard', [DashboardCotroller::class, 'index'])->name('dashboard')->middleware('auth');
 
 
-// pegawai
-Route::get('pegawai', [PegawaiController::class, 'index'])->name('pegawai.index');
-Route::get('create/pegawai', [PegawaiController::class, 'create'])->name('pegawai.create');
-Route::post('pegawai', [PegawaiController::class, 'store'])->name('pegawai.store');
-Route::get('pegawai/{id}/edit', [PegawaiController::class, 'edit'])->name('pegawai.edit');
-Route::put('pegawai/{id}', [PegawaiController::class, 'update'])->name('pegawai.update');
-Route::get('pegawai/{id}', [PegawaiController::class, 'destroy'])->name('pegawai.destroy');
-Route::get('pegawais/count', [PegawaiController::class, 'show'])->name('pegawai.count');
+Route::group(['middleware' => 'auth'], function () {
 
-// bendera
-Route::get('bendera', [BenderaController::class, 'index'])->name('bendera.index');
-Route::get('create/bendera', [BenderaController::class, 'create'])->name('bendera.create');
-Route::post('bendera', [BenderaController::class, 'store'])->name('bendera.store');
-Route::get('bendera/{id}/edit', [BenderaController::class, 'edit'])->name('bendera.edit');
-Route::put('bendera/{id}', [BenderaController::class, 'update'])->name('bendera.update');
-Route::get('bendera/{id}', [BenderaController::class, 'destroy'])->name('bendera.destroy');
+    Route::get('/dashboard', [DashboardCotroller::class, 'index'])->name('dashboard');
 
-// perusahaan
-Route::get('perusahaan', [PerusahaanController::class, 'index'])->name('perusahaan.index');
-Route::get('create/perusahaan', [PerusahaanController::class, 'create'])->name('perusahaan.create');
-Route::post('perusahaan', [PerusahaanController::class, 'store'])->name('perusahaan.store');
-Route::get('perusahaan/{id}/edit', [PerusahaanController::class, 'edit'])->name('perusahaan.edit');
-Route::put('perusahaan/{id}', [PerusahaanController::class, 'update'])->name('perusahaan.update');
-Route::get('perusahaan/{id}', [PerusahaanController::class, 'destroy'])->name('perusahaan.destroy');
+    // pegawai
+    Route::get('pegawai', [PegawaiController::class, 'index'])->name('pegawai.index');
+    Route::get('create/pegawai', [PegawaiController::class, 'create'])->name('pegawai.create');
+    Route::post('pegawai', [PegawaiController::class, 'store'])->name('pegawai.store');
+    Route::get('pegawai/{id}/edit', [PegawaiController::class, 'edit'])->name('pegawai.edit');
+    Route::put('pegawai/{id}', [PegawaiController::class, 'update'])->name('pegawai.update');
+    Route::get('pegawai/{id}', [PegawaiController::class, 'destroy'])->name('pegawai.destroy');
+    Route::get('pegawais/count', [PegawaiController::class, 'show'])->name('pegawai.count');
 
-// kapal
-Route::get('kapal', [KapalController::class, 'index'])->name('kapal.index');
-Route::get('create/kapal', [KapalController::class, 'create'])->name('kapal.create');
-Route::post('kapal', [KapalController::class, 'store'])->name('kapal.store');
-Route::get('kapal/{id}/edit', [KapalController::class, 'edit'])->name('kapal.edit');
-Route::put('kapal/{id}', [KapalController::class, 'update'])->name('kapal.update');
-Route::get('kapal/{id}', [KapalController::class, 'destroy'])->name('kapal.destroy');
-// mendapatkan id yang berada di kapal
-Route::get('/get-data/{kapal_id}', [KapalController::class, 'getData'])->name('kapal.getData');
+    // bendera
+    Route::get('bendera', [BenderaController::class, 'index'])->name('bendera.index');
+    Route::get('create/bendera', [BenderaController::class, 'create'])->name('bendera.create');
+    Route::post('bendera', [BenderaController::class, 'store'])->name('bendera.store');
+    Route::get('bendera/{id}/edit', [BenderaController::class, 'edit'])->name('bendera.edit');
+    Route::put('bendera/{id}', [BenderaController::class, 'update'])->name('bendera.update');
+    Route::get('bendera/{id}', [BenderaController::class, 'destroy'])->name('bendera.destroy');
 
+    // perusahaan
+    Route::get('perusahaan', [PerusahaanController::class, 'index'])->name('perusahaan.index');
+    Route::get('create/perusahaan', [PerusahaanController::class, 'create'])->name('perusahaan.create');
+    Route::post('perusahaan', [PerusahaanController::class, 'store'])->name('perusahaan.store');
+    Route::get('perusahaan/{id}/edit', [PerusahaanController::class, 'edit'])->name('perusahaan.edit');
+    Route::put('perusahaan/{id}', [PerusahaanController::class, 'update'])->name('perusahaan.update');
+    Route::get('perusahaan/{id}', [PerusahaanController::class, 'destroy'])->name('perusahaan.destroy');
 
-// tipe kapal
-Route::get('tipe_kapal', [TipeKapalController::class, 'index'])->name('tipe_kapal.index');
-Route::get('create/tipe_kapal', [TipeKapalController::class, 'create'])->name('tipe_kapal.create');
-Route::post('tipe_kapal', [TipeKapalController::class, 'store'])->name('tipe_kapal.store');
-Route::get('tipe_kapal/{id}/edit', [TipeKapalController::class, 'edit'])->name('tipe_kapal.edit');
-Route::put('tipe_kapal/{id}', [TipeKapalController::class, 'update'])->name('tipe_kapal.update');
-Route::get('tipe_kapal/{id}', [TipeKapalController::class, 'destroy'])->name('tipe_kapal.destroy');
+    // kapal
+    Route::get('kapal', [KapalController::class, 'index'])->name('kapal.index');
+    Route::get('create/kapal', [KapalController::class, 'create'])->name('kapal.create');
+    Route::post('kapal', [KapalController::class, 'store'])->name('kapal.store');
+    Route::get('kapal/{id}/edit', [KapalController::class, 'edit'])->name('kapal.edit');
+    Route::put('kapal/{id}', [KapalController::class, 'update'])->name('kapal.update');
+    Route::get('kapal/{id}', [KapalController::class, 'destroy'])->name('kapal.destroy');
+    // mendapatkan id yang berada di kapal
+    Route::get('/get-data/{kapal_id}', [KapalController::class, 'getData'])->name('kapal.getData');
 
-// pelabuhan
-Route::get('pelabuhan', [PelabuhanController::class, 'index'])->name('pelabuhan.index');
-Route::get('create/pelabuhan', [PelabuhanController::class, 'create'])->name('pelabuhan.create');
-Route::post('pelabuhan', [PelabuhanController::class, 'store'])->name('pelabuhan.store');
-Route::get('pelabuhan/{id}/edit', [PelabuhanController::class, 'edit'])->name('pelabuhan.edit');
-Route::put('pelabuhan/{id}', [PelabuhanController::class, 'update'])->name('pelabuhan.update');
-Route::get('pelabuhan/{id}', [PelabuhanController::class, 'destroy'])->name('pelabuhan.destroy');
+    // tipe kapal
+    Route::get('tipe_kapal', [TipeKapalController::class, 'index'])->name('tipe_kapal.index');
+    Route::get('create/tipe_kapal', [TipeKapalController::class, 'create'])->name('tipe_kapal.create');
+    Route::post('tipe_kapal', [TipeKapalController::class, 'store'])->name('tipe_kapal.store');
+    Route::get('tipe_kapal/{id}/edit', [TipeKapalController::class, 'edit'])->name('tipe_kapal.edit');
+    Route::put('tipe_kapal/{id}', [TipeKapalController::class, 'update'])->name('tipe_kapal.update');
+    Route::get('tipe_kapal/{id}', [TipeKapalController::class, 'destroy'])->name('tipe_kapal.destroy');
 
-//spb
-Route::get('spb', [SpbController::class, 'index'])->name('spb.index');
-Route::get('create/spb', [SpbController::class, 'create'])->name('spb.create');
-Route::post('spb', [SpbController::class, 'store'])->name('spb.store');
-Route::get('spb/{id}/edit', [SpbController::class, 'edit'])->name('spb.edit');
-Route::put('spb/{id}', [SpbController::class, 'update'])->name('spb.update');
-Route::delete('spb/{id}', [SpbController::class, 'destroy'])->name('spb.destroy');
-Route::get('spb/{id}', [SpbController::class, 'show'])->name('spb.show');
+    // pelabuhan
+    Route::get('pelabuhan', [PelabuhanController::class, 'index'])->name('pelabuhan.index');
+    Route::get('create/pelabuhan', [PelabuhanController::class, 'create'])->name('pelabuhan.create');
+    Route::post('pelabuhan', [PelabuhanController::class, 'store'])->name('pelabuhan.store');
+    Route::get('pelabuhan/{id}/edit', [PelabuhanController::class, 'edit'])->name('pelabuhan.edit');
+    Route::put('pelabuhan/{id}', [PelabuhanController::class, 'update'])->name('pelabuhan.update');
+    Route::get('pelabuhan/{id}', [PelabuhanController::class, 'destroy'])->name('pelabuhan.destroy');
+
+    //spb
+    Route::get('spb', [SpbController::class, 'index'])->name('spb.index');
+    Route::get('create/spb', [SpbController::class, 'create'])->name('spb.create');
+    Route::post('spb', [SpbController::class, 'store'])->name('spb.store');
+    Route::get('spb/{id}/edit', [SpbController::class, 'edit'])->name('spb.edit');
+    Route::put('spb/{id}', [SpbController::class, 'update'])->name('spb.update');
+    Route::delete('spb/{id}', [SpbController::class, 'destroy'])->name('spb.destroy');
+    Route::get('spb/{id}', [SpbController::class, 'show'])->name('spb.show');
+
+    //signout
+    Route::get('logout', [LoginController::class, 'logout'])->name('logout');
+});

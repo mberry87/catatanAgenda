5cdf874209ae4415c96724ed61500a092a27d673
mberry87 <berry311287@gmail.com>
2023-06-09 17:41:03 +0700

    update profile file

--- resources/views/backend/profil/index.blade.php
+++ resources/views/backend/profil/index.blade.php
@@ -20,52 +20,60 @@
     <div class="row">
         <div class="col-md-6">
             <div class="card">
+                @if (session('success'))
+                    <div class="alert alert-success" role="alert">
+                        {{ session('success') }}
+                    </div>
+                @endif
                 <div class="card-body">
                     <div class="form-group">
                         <div class="text-center">
                             <img class="profile-user-img img-fluid img-circle"
-                                src="{{ asset('template') }}/dist/img/user4-128x128.jpg" alt="User profile picture">
-                            <h3 class="profile-username text-center">Nina Mcintire</h3>
-                            <p class="text-muted text-center">Nip : </p>
-                            <p class="text-muted text-center">Golongan : </p>
-                            <p class="text-muted text-center">Status : </p>
+                                src="{{ asset('storage/photo_profil/' . $userData->photo_profil) }}"
+                                alt="User profile picture">
+                            <h3 class="profile-username text-center">{{ $userData->name }}</h3>
+                            <p class="text-muted text-center">Email : {{ $userData->email }}</p>
+                            <p class="text-muted text-center">Tempat Tanggal Lahir : </p>
+                            <p class="text-muted text-center">Agama : </p>
                         </div>
-                        <div class="mb-3">
-                            <label for="formFile" class="form-label">Upload foto profil</label>
-                            <input class="form-control" type="file" id="formFile">
-                        </div>
+                        <form action="{{ route('photoProfil') }}" method="POST" enctype="multipart/form-data">
+                            @csrf
+                            <div class="mb-3">
+                                <label for="photo_profil" class="form-label ">Upload foto profil</label>
+                                <input name="photo_profil" class="form-control" type="file" id="photo_profil">
+                            </div>
+                            <button type="submit" class="btn btn-primary btn-sm ">Update foto</button>
+                        </form>
                     </div>
-                    <button type="submit" class="btn btn-primary btn-sm ">Update foto</button>
                 </div>
             </div>
         </div>
         <div class="col-md-6">
             <div class="card">
                 <div class="card-header">
-                    <h5 class="m-0">Info Profil</h5>
+                    <h5 class="m-0">Edit Profil</h5>
                 </div>
                 <div class="card-body">
                     <form action="#">
-                        @csrf
-                        {{-- @method(PUT) --}}
-
-                        {{-- <div class="form-group">
+                        <div class="form-group">
                             <label for="nama">Nama:</label>
                             <input type="text" class="form-control">
                         </div>
                         <div class="form-group">
                             <label for="nip">NIP:</label>
                             <input type="text" class="form-control">
-                        </div> --}}
+                        </div>
                         <div class="form-group">
-                            <label for="tempat_lahir">Tempat Lahir:</label>
-                            <input type="text" class="form-control">
+                            <label for="tmp_lahir" for="tempat_lahir">Tempat Lahir:</label>
+                            <input name="name" id="name" type="text" class="form-control"
+                                value="{{ $userData->name }}">
                         </div>
                         <div class="form-group">
                             <label for="tgl_lahir">Tanggal Lahir:</label>
-                            <input type="text" class="form-control">
+                            <input name="tgl_lahir" id="tgl_lahir" type="text" class="form-control"
+                                value="{{ $userData->tgl_lahir }}">
                         </div>
-                        {{-- <div class="form-group">
+                        <div class="form-group">
                             <label for="Alamat">Alamat:</label>
                             <input type="text" class="form-control">
                         </div>
@@ -76,14 +84,16 @@
                         <div class="form-group">
                             <label for="telepon">Telepon:</label>
                             <input type="text" class="form-control">
-                        </div> --}}
-                        <div class="form-group">
-                            <label for="agama">Agama:</label>
-                            <input type="text" class="form-control">
                         </div>
                         <div class="form-group">
-                            <label for="email">Email:</label>
-                            <input type="email" class="form-control">
+                            <label for="agama">Agama:</label>
+                            <input name="agama" id="agama" type="text" class="form-control"
+                                value="{{ $userData->agama }}">
+                        </div>
+                        <div class="form-group">
+                            <label>Email:</label>
+                            <input name="email" id="email" type="email" class="form-control"
+                                value="{{ $userData->email }}">
                         </div>
                         <button type="submit" class="btn btn-primary btn-sm">Update Profil</button>
                         <a href="#" class="btn btn-warning btn-sm">Kembali</a>
